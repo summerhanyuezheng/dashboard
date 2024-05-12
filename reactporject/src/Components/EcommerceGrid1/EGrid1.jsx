f01eb92329b770039ca8c7f2218397e3e640e9aa
summerhanyuezheng <summersleepy69@gmail.com>
2024-05-11 21:29:17 -0700

    finished grid1 for ecommerce page

--- reactporject/src/Components/EcommerceGrid1/EGrid1.jsx
+++ reactporject/src/Components/EcommerceGrid1/EGrid1.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './EGrid1.css'
+import NewVisitorChart from './NewVisitorChart'
+import ActivityChart from './ActivityChart'
 
 export default function EGrid1() {
   return (
@@ -18,6 +20,41 @@ export default function EGrid1() {
               </div>
             </div>
             <div className='right-row-one grid-style'>
+              <div className='new-visitor'>
+                
+                <div className='new-visitor-top-flex'> 
+                <h2 className='each-title'>New Vistiors</h2>
+                <h4 className='top-grey-bold-sub '>Last Week </h4>
+                  
+                </div>
+                <div className='new-visitor-bottom-flex'>
+                  <div className='stack-num'> 
+                    <h1  className='percent-big'>23%</h1>
+                    <h5 className='percent-num' style={{color:'red'}}>↓ 8.75</h5>
+                  </div>
+                    <div className='visitor-chart'><NewVisitorChart></NewVisitorChart></div>
+                  
+                 
+                </div>
+               
+              </div>
+              <div className='activity'>
+              <div className='activity-top-flex'> 
+                <h2 className='each-title'>Activity</h2>
+                <h4 className='top-grey-bold-sub '>Last Week </h4>
+                  
+                </div>
+                <div className='activity-bottom-flex'>
+                  <div className='stack-num'> 
+                    <h1  className='percent-big'>82%</h1>
+                    <h5 className='percent-num' >↑ 19.6%</h5>
+                  </div>
+                    <div className='visitor-chart'><ActivityChart></ActivityChart></div>
+                  
+                 
+                </div>
+               
+              </div>
               
             </div>
 
